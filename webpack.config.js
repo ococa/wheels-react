1f06d7d8127fd7302168ec305dd964eb489ffbe3
ococa <wangchao.personal@gmail.com>
2020-01-02 17:04:28 +0800

    chore: add ts

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    index: './lib/index.tsx',
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist/lib'),
+    library: 'cuu', // 配置库的名字,
+    libraryTarget: 'umd',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'awesome-typescript-loader',
+        include: path.resolve(__dirname, 'lib'),
+        exclude: path.resolve(__dirname, 'node_modules')
+      }
+    ]
+  }
+};
