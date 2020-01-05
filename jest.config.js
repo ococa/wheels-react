fe1fd5bd65c340cde88f364c1e2fd2748caa1678
ococa <wangchao.personal@gmail.com>
2020-01-05 17:25:12 +0800

    fix: fix ** path

--- jest.config.js
+++ jest.config.js
@@ -17,7 +17,7 @@ module.exports = {
         "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/test/__mocks__/file-mock.js",
         "\\.(scss|css|sass|less)$": "<rootDir>/test/__mocks__/object-mock.js",
     },
-    testMatch: ['<rootDir>/**/__test__/**/.test.(js|jsx|ts|tsx)'],
+    testMatch: ['<rootDir>/**/__test__/**/*.test.(js|jsx|ts|tsx)'],
     transform: {
       "^.+\\.(js|jsx)$": "babel-jest",
       '^.+\\.(ts|tsx)$': 'ts-jest',
