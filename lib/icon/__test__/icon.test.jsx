4bd5e14021176c2d147495be45e4713667f39af5
ococa <wangchao.personal@gmail.com>
2020-01-05 13:51:59 +0800

    feat: import enzyme to test click event

--- lib/icon/__test__/icon.test.jsx
+++ lib/icon/__test__/icon.test.jsx
@@ -1,10 +1,18 @@
-import * as renderer from 'react-test-renderer'
-import React from 'react'
+import * as renderer from 'react-test-renderer';
+import React from 'react';
+import { mount } from 'enzyme';
 import Icon from '../index';
 
 describe('button', () => {
   it('是个 div', () => {
-    const json = renderer.create(<Icon />).toJSON()
+    const json = renderer.create(<Icon />).toJSON();
     expect(json).toMatchSnapshot()
+  });
+
+  it('onClick', () => {
+    const fn = jest.fn();
+    const component = mount(<Icon name="alipay" onClick={fn}/>);
+    component.find('svg').simulate('click');
+    expect(fn).toBeCalled()
   })
 });
